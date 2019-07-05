a945a3053f771e927b5065ae4cda0235d82aa2a0
Hugo Gabriel <hugo.silva@ccc.ufcg.edu.br>
2019-07-04 21:05:30 -0300

    Atualiza script para extração dos dados e o script para transformação .Rmd>.R

--- scripts/rmd-to-r.R
+++ scripts/rmd-to-r.R
@@ -5,3 +5,12 @@ r_path <- paste(base_path, "producao-e-produtividade.R", sep='')
 
 knitr::purl(rmd_path, r_path, documentation = 0)
 
+
+
+paths_list = "path dos arquivos .Rmd para serem transformados em R.Rmd"
+
+for (path in paths_list) {
+  r_file_path = substr(path, 1, nchar(path)-2)
+  knitr::purl(path, r_path, documentation = 0)
+}
+
